5ff00cf94174ffcf625d3b11e6ff51132fee9358
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2018-04-19 19:39:10 +0000

    final version of sql query for SQL Server

--- querytest.sql
+++ querytest.sql
@@ -1,4 +1,3 @@
-
 -- Declare variables; only once per script
 declare @startdate datetime
 declare @enddate datetime
@@ -8,10 +7,11 @@ declare @run2 int=0
 declare @value int=1000
 
 declare @output table (r1_value INT, run INT, startdate datetime, enddate datetime, timed numeric(15,3))
+declare @temp table (temp_v INT)
 
 WHILE @run2 < 24
 BEGIN
--- Clean buffers
+-- Clean buffers; need to do it for each value
 DBCC DROPCLEANBUFFERS
 DBCC FREEPROCCACHE
 
@@ -20,23 +20,26 @@ DBCC FREEPROCCACHE
 		-- Starting time
 		set @startdate=GETDATE()
 
-		-- Query
+		-- Query; inserting results into a table to lower result clutter
+		INSERT INTO @temp (temp_v)
+		--SELECT COUNT(UNIQUE1)
+		--FROM Wisc1000K WITH (INDEX(IX_Wisc1000Ka)) --trying to force unclustered
+		--SELECT COUNT(UNIQUE2)
+		--FROM Wisc1000K WITH (INDEX(PK_Wisc1000Ka)) --trying to force clustered
 		SELECT COUNT(UNIQUE3)
-		from Wisc1000K
-		--where ONE_PERC < 50
-		--where TWENTY_PERC < 4
-		--where R1 < 200000
-		where R1 < @value
+		FROM Wisc1000K WITH (INDEX(0)) --trying to force table scan
+		WHERE R1 < @value
 
-		--- End time; print the difference
+		--- End time; put results into a table
 		set @enddate=GETDATE()
 		set @time=(CONVERT(numeric(15,8), @enddate)-CONVERT(numeric(15,8), @startdate))*(24.0*60.0*60.0)
-		--select @run, @startdate, @enddate, @time
 		INSERT into @output (r1_value, run, startdate, enddate, timed)
 		VALUES (@value, @run, @startdate, @enddate, @time)
 
 		SET @run = @run + 1
 	END;
+
+--- Value check; changing value so it will increment up to 1 million in different increments
 if @value < 10000
 	SET @value = @value + 1000
 else if @value < 50000
@@ -48,5 +51,7 @@ else
 SET @run2 = @run2 + 1
 SET @run = 0
 END;
+
+--- Display results
 select r1_value, run, startdate, enddate, timed
 from @output
