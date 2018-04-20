2b4f77771cd304c09a2a2d62c88d939e19d5f9e8
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2018-04-20 04:28:15 +0000

    loops work and results are shown but missing proper measurements for each query, everything is shown with same timestamp

--- post_test.sql
+++ post_test.sql
@@ -2,9 +2,6 @@ drop table if exists outputs;
 drop table if exists temps;
 drop function if exists test();
 
-create temp table outputs(r1_value int, run int, startdate timestamp(3), enddate timestamp(3), timed numeric(15,3));
-create temp table temps(temp_v int);
-
 create or replace function test()
 returns
 	table (r int, run int, startdate timestamp(3), enddate timestamp(3), timed numeric(15,3)) as $$
@@ -16,6 +13,8 @@ declare
 	v_run2 int=0;
 	v_value int=0;
 BEGIN
+	create temp table outputs(r1_value int, run int, startdate timestamp(3), enddate timestamp(3), timed numeric(15,3));
+	create temp table temps(temp_v int);
 	WHILE v_run2 < 25
 	LOOP
 		WHILE v_run < 5
@@ -27,7 +26,7 @@ BEGIN
 			where unique3 <= v_value;
 			v_enddate:=NOW();
 			--v_time:=(CONVERT(numeric(15,8), v_enddate)-CONVERT(numeric(15,8), v_startdate))*(24.0*60.0*60.0);
-			v_time:=(EXTRACT(MILLISECONDS FROM v_enddate)-EXTRACT(MILLISECODS FROM v_startdate));
+			v_time:=(EXTRACT(MILLISECONDS FROM v_enddate)-EXTRACT(MILLISECONDS FROM v_startdate));
 			insert into outputs(r1_value, run, startdate, enddate, timed)
 			values (v_value, v_run, v_startdate, v_enddate, v_time);
 			
@@ -35,9 +34,9 @@ BEGIN
 		END LOOP;
 	if v_value < 10000 then
 		v_value := v_value + 1000;
-	else if v_value < 50000 then
+	elsif v_value < 50000 then
 		v_value := v_value + 10000;
-	else if v_value < 100000 then
+	elsif v_value < 100000 then
 		v_value := v_value + 50000;
 	else
 		v_value := v_value + 100000;
@@ -45,9 +44,11 @@ BEGIN
 	v_run2 := v_run2 + 1;
 	v_run := 0;
 	END LOOP;
-RETURN outputs;
+RETURN QUERY SELECT * FROM outputs;
+drop table if exists outputs;
+drop table if exists temps;
 END;
 $$ LANGUAGE plpgsql;
 
 select * from test();
-			
+--select * from outputs;			
